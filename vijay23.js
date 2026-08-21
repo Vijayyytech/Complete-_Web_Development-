94d6ea481ca0b0a41ada24109738ea283765cc3a
Vijayyytech <vijaytripathi1296@gmail.com>
2026-08-21 23:28:05 +0530

    Initial commit for web development

---
+++ vijay23.js
@@ -0,0 +1,16 @@
+/* console.log("vijju tripathi");
+function sum(a,b,c){
+    let d = a+b+c;
+    return d;
+}
+
+let result = sum(4,3,5);
+console.log(result); */
+
+function greet(name){
+    console.log('good morning' + ' ' + name);
+}
+
+let value = 'vijju';
+greet(value);
+greet('manga');
